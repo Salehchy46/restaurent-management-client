62b30bdcdce70bf2e97ff949b53e8337f714d614
salehchy46 <salehchyctg@gmail.com>
2025-07-10 01:28:09 +0600

    my footer is added

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -4,14 +4,14 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
 
     const navLinks = <>
-        <li className='hover:border-b-2'><NavLink className='hover:bg-white' to='/'>Home</NavLink></li>
+        <li className='hover:border-b-2'><NavLink className='hover:bg-white active:border-b-2' to='/'>Home</NavLink></li>
         <li className='hover:border-b-2'><NavLink className='hover:bg-white' to='/menu'>Menu</NavLink></li>
         <li className='hover:border-b-2'><NavLink className='hover:bg-white' to='/'>Home</NavLink></li>
         <li className='hover:border-b-2'><NavLink className='hover:bg-white' to='/'>Home</NavLink></li>
     </>
 
     return (
-        <div className="navbar shadow-sm">
+        <div className="navbar shadow-sm bg-base-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,7 +23,7 @@ const Header = () => {
                         {navLinks}
                     </ul>
                 </div>
-                <img src="https://i.ibb.co/XkZkv2kw/download.webp" alt="logo" className='w-20' />
+                <img src="https://i.ibb.co/XkZkv2kw/download.webp" alt="logo" className='w-20 rounded-full' />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
