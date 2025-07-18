a713794bdf681e039ff1fcfa83b43a738622ecb7
salehchy46 <salehchyctg@gmail.com>
2025-07-18 22:52:35 +0600

    Login process is completed

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,27 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AuthContext from '../../contexts/AuthContext/AuthContext';
 import Lottie from 'lottie-react';
 import login from '../../../assets/lotties/login.json'
 
 const Login = () => {
 
-    const {signIn} = useContext(AuthContext);
+    const { signIn } = useContext(AuthContext);
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const handleLogin = e => {
         e.preventDefault();
+
+        setError('');
+        setSuccess('');
+
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
 
         console.log(email, password);
 
+        if (!email || !password) {
+            setError('Email and password are required.');
+            return;
+        }
+
         signIn(email, password)
-        .then(result => {
-            console.log(result.user.email);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+            .then(result => {
+                console.log(result.user.email);
+                setSuccess('Login successful!')
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+            })
     }
 
     return (
@@ -29,6 +43,8 @@ const Login = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div>
                     <Lottie className='w-96' animationData={login}></Lottie>
+                    <p className="text-red-600 ml-4 text-center">{error}</p>
+                    <p className="text-green-600 ml-4 text-center">{success}</p>
                 </div>
                 <div className="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl">
                     <h1 className="text-5xl font-bold">Log-in now!</h1>
